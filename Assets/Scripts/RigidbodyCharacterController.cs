d08e1202c56021b425c750cf7a416011bd9c85f3
379146 <379146@colum.edu>
2020-10-08 00:48:45 -0500

    Cleaned Code in player controller

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -15,71 +15,16 @@ public class RigidbodyCharacterController : MonoBehaviour
     [SerializeField]
     private float speedMult = 0.01F;
 
-    //[SerializeField]
-    //private float maxSpeed = 8;
-
-    //[SerializeField]
-    //private PhysicMaterial stoppingPhysicsMaterial, movingPhysicsMaterial;
-
-    //[SerializeField]
-    //[Tooltip("0 = no turning, 1 = Instant turning")]
-    //[Range(0, 1)]
-    //private float turnSpeed = 0.1f;
-
-    private new Rigidbody rigidbody;
     private Vector2 input;
-    private new Collider collider;
-
-    private void Start()
-    {
-
-        rigidbody = GetComponent<Rigidbody>();
-        collider = GetComponent<Collider>();
-
-    }
 
     private void FixedUpdate()
     {
 
-        //var inputDirection = new Vector3(input.x, 0, input.y);
         var inputDirection = new Vector3(input.x, 0, forwardSpeed);
 
         transform.Translate(inputDirection * speed);
 
         forwardSpeed = forwardSpeed + speedMult;
-
-        //Vector3 cameraFlattenedForward = Camera.main.transform.forward;
-        //cameraFlattenedForward.y = 0;
-        //Quaternion cameraRotation = Quaternion.LookRotation(cameraFlattenedForward);
-
-        //Vector3 cameraRelativeInputDirection = cameraRotation * inputDirection;
-
-        //if (inputDirection.magnitude > 0)
-        //{
-
-        //    collider.material = movingPhysicsMaterial;
-
-        // }
-        //else
-        //{
-        //    collider.material = stoppingPhysicsMaterial;
-        //}
-        //if (rigidbody.velocity.magnitude < maxSpeed)
-        //{
-        
-            //rigidbody.AddForce(inputDirection * accelerationForce, ForceMode.Acceleration);
-
-        //    transform.Translate(inputDirection * accelerationForce);
-
-        //}
-
-        //if (cameraRelativeInputDirection.magnitude > 0)
-        //{
-
-        //    var targetRotation = Quaternion.LookRotation(cameraRelativeInputDirection);
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed);
-
-        //}
 
     }
 
@@ -87,7 +32,6 @@ public class RigidbodyCharacterController : MonoBehaviour
     {
 
         input.x = Input.GetAxisRaw("Horizontal");
-        //input.y = Input.GetAxisRaw("Vertical");
     
     }
 
