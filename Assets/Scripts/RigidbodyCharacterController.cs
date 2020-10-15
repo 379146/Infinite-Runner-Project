bd466d405a0e5ede5e569901c2774198bc8b6e15
379146 <379146@colum.edu>
2020-10-15 00:24:06 -0500

    Added jumping
    I dont really like it though

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -8,6 +8,9 @@ public class RigidbodyCharacterController : MonoBehaviour
 
     [SerializeField]
     private float speed = 10;
+    
+    [SerializeField]
+    private float jumpPower = 5;
 
     [SerializeField]
     private float forwardSpeed = 1;
@@ -16,6 +19,15 @@ public class RigidbodyCharacterController : MonoBehaviour
     private float speedMult = 0.01F;
 
     private Vector2 input;
+    private new Rigidbody rigidbody;
+
+    private void Start()
+    {
+
+        rigidbody = GetComponent<Rigidbody>();
+        //collider = GetComponent<Collider>();
+
+    }
 
     private void FixedUpdate()
     {
@@ -32,7 +44,13 @@ public class RigidbodyCharacterController : MonoBehaviour
     {
 
         input.x = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(0,jumpPower,0, ForceMode.Impulse);
+        }
     
     }
+
 
 }
