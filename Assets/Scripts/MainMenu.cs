f09b6abe32da3e23b94a00d2f660d66b64e4fb9b
379146 <379146@colum.edu>
2020-12-17 00:12:02 -0600

    Levels structure implemented
    Time to flesh them out

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,18 +6,39 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
-    public void PlayGame()
-    {
+    //public void PlayGame()
+    //{
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     
-    }
+    //}
 
     public void QuitGame()
     {
 
         Application.Quit();
     
+    }
+
+    public void StartLevel1()
+    {
+
+        SceneManager.LoadScene("Level_1");
+
+    }
+
+    public void StartLevel2()
+    {
+
+        SceneManager.LoadScene("Level_2");
+
+    }
+
+    public void StartLevel3()
+    {
+
+        SceneManager.LoadScene("Level_3");
+
     }
 
 }
