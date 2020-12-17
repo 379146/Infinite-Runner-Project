79c148078ff31ee08f18de865094920509bebb1d
379146 <379146@colum.edu>
2020-12-17 01:03:48 -0600

    Level Text appears at start of each level

---
+++ Assets/Scripts/LevelTextController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class LevelTextController : MonoBehaviour
+{
+
+    public TMP_Text LevelText;
+    public int LevelNumber;
+    public bool startfade = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        LevelNumber = SceneManager.GetActiveScene().buildIndex;
+
+        LevelText.text = "Level " + LevelNumber;
+
+        Invoke("BeginFade", 2f);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        while (startfade && LevelText.alpha >= 0)
+        {
+            LevelText.alpha -= 0.01f;
+            Debug.Log(LevelText.alpha);
+        }
+
+    }
+
+    void BeginFade()
+    {
+
+        startfade = true;
+    
+    }
+
+}
